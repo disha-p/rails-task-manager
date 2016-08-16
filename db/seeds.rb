10553a42be76c9f623cc11cb6803313890ba8cdf
disha patel <disha.p@hotmail.co.uk>
2016-08-16 15:25:22 +0100

    it is built

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(name: "groceries", completed: "false")
+Task.create(name: "laundry", completed: "true")
+Task.create(name: "cook dinner", completed: "false")
